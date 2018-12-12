d22aabb238ba421d7d8a253cce00eae72842458d
Yokesh <byokeshb@gmail.com>
2018-12-12 22:26:35 +0530

    Lifting state and final updates

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component{
+
+
+
+
+
+  render() {
+
+    return (
+      <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="#"><span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span></a>
+  </nav>
+    );
+  }
+
+}
+
+
+
+export default Navbar;
