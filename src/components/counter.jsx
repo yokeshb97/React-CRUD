bf8c0c19ce800cea2a87cf97d017da8db865ea26
Yokesh <byokeshb@gmail.com>
2018-12-12 21:15:31 +0530

    Rendering multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,10 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
-  state={
-    count:0,
-    tags:["tag1","tag2","tag3"]
-  };
 
 
-  handleIncrement=()=>{
 
-    this.setState({count :this.state.count+1});
-  }
+
 
   render() {
 
@@ -18,20 +12,19 @@ class Counter extends Component{
       <div>
 
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
-        <ul>{this.state.tags.map((tag)=><li key={tag}>{tag}</li>)}
-        </ul>
+        <button onClick={()=>this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm"> Increment </button>
+        <button  onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
   getBadgeClasses(){
     let classes="badge m-2 badge-";
-    classes += (this.state.count===0)?"warning":"primary";
+    classes += (this.props.counter.value===0)?"warning":"primary";
     return classes;
   }
   formatCount(){
-    const {count}=this.state;
-    return count === 0?'Zero':count;
+    const {value}=this.props.counter;
+    return value === 0?'Zero':value;
   }
 }
 
