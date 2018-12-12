44462aad40dc92203aae154550cd9e84dc938876
Yokesh <byokeshb@gmail.com>
2018-12-12 12:08:15 +0530

    State changes in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,13 +6,19 @@ class Counter extends Component{
     tags:["tag1","tag2","tag3"]
   };
 
+
+  handleIncrement=()=>{
+
+    this.setState({count :this.state.count+1});
+  }
+
   render() {
 
     return (
       <div>
 
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
-        <button className="btn btn-secondary btn-sm"> Increment </button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
         <ul>{this.state.tags.map((tag)=><li key={tag}>{tag}</li>)}
         </ul>
       </div>
